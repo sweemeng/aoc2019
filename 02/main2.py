bd2fc6d48a4a039fb081841c004473a397b92b89
sweemeng <sweester@gmail.com>
2019-12-03 20:55:29 +0800

    Commit day 2 solution

---
+++ 02/main2.py
@@ -0,0 +1,50 @@
+import operator
+
+
+def interpreter(codes, trace=False):
+    pointer = 0
+    while True:
+        ops = {1: operator.add, 2: operator.mul}
+        op_code = codes[pointer]
+        if op_code == 99:
+            if trace:
+                print("Halt")
+            break
+        pos1 = codes[pointer+1]
+        pos2 = codes[pointer+2]
+        values = (codes[pos1], codes[pos2])
+        output = codes[pointer+3]
+        codes[output] = ops[op_code](*values)
+        if trace:
+            print("op_code: ", op_code)
+            print("pos1: ", pos1)
+            print("value1: ", codes[pos1])
+            print("pos2: ", pos2)
+            print("value2: ", codes[pos2])
+            print("result: ", output)
+        pointer += 4
+
+    return codes
+
+
+def main():
+    f = open("input")
+    text = f.read()
+    data = [int(i) for i in text[:-1].split(',')]
+    result = 0
+    for noun in range(1, 100):
+        for verb in range(1,100):
+            code = data[:]
+            code[1] = noun
+            code[2] = verb
+            interpreter(code)
+            if code[0] == 19690720:
+                result = noun * 100 + verb
+                break
+        if result:
+            break
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
